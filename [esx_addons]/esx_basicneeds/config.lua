b4b5b2407c976d341f7915216438329406c9d276
James Lepone <jameslepone@gmail.com>
2022-12-08 21:18:19 -0500

    Updated

--- [esx_addons]/esx_basicneeds/config.lua
+++ [esx_addons]/esx_basicneeds/config.lua
@@ -4,14 +4,54 @@ Config.Visible = true
 
 Config.Food = {
 	["bread"] = {
+		status = 80000,
+		remove = true
+	},
+  ["taco"] = {
+		status = 100000,
+		remove = true
+	},
+  ["hotdog"] = {
+		status = 150000,
+		remove = true
+	},
+  ["hamburger"] = {
 		status = 200000,
+		remove = true
+	},
+  ["pizza_slice"] = {
+		status = 150000,
+		remove = true
+	},
+  ["chips"] = {
+		status = 50000,
+		remove = true
+	},
+  ["fried_chicken"] = {
+		status = 350000,
 		remove = true
 	}
 }
 
 Config.Drinks = {
 	["water"] = {
+		status = 500000,
+		remove = true
+	},
+  ["milk"] = {
 		status = 100000,
+		remove = true
+	},
+  ["energy_drink"] = {
+		status = 250000,
+		remove = true
+	},
+  ["cola"] = {
+		status = 50000,
+		remove = true
+	},
+  ["sprite"] = {
+		status = 50000,
 		remove = true
 	}
 }
